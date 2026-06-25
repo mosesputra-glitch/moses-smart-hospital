28c7327421e6c5df5fe95dd34f751715e7c52cbc
mosesputra-glitch <moses.putra@tzuchi.sch.id>
2026-06-25 14:08:03 +0700

    e

--- hospital_app.py
+++ hospital_app.py
@@ -170,7 +170,7 @@ with st.form("triage_form"):
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
             <span style="background:#25f20a;color:white;border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">3</span>
-            <span style="font-size:16px;font-weight:600;color:#105efa;">CODENO17</span>
+            <span style="font-size:16px;font-weight:600;color:#105efa;">how would you the severyty</span>
         </div>
     </div>
     """, unsafe_allow_html=True)
